5973f3edd9c5fad09137580dfb4489ffba9f2456
David Massenot <david@artcustomer.fr>
2025-02-14 13:38:51 +0100

    List audio formats

---
+++ src/Enum/AudioFormat.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Artcustomer\OpenAIClient\Enum;
+
+/**
+ * @author David
+ */
+class AudioFormat
+{
+
+    public const AAC = 'aac';
+    public const FLAC = 'flac';
+    public const MP3 = 'mp3';
+    public const OPUS = 'opus';
+    public const PCM = 'pcm';
+    public const WAV = 'wav';
+
+}
